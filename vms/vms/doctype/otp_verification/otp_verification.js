8b7a36be3fbc29f4ac44d850f7171453cb263a0e
Nitesh4813 <goadniteshmeril4813@gmail.com>
2025-05-07 16:15:35 +0530

    created otp verification api

--- vms/vms/doctype/otp_verification/otp_verification.js
+++ vms/vms/doctype/otp_verification/otp_verification.js
@@ -7,3 +7,15 @@
 // 	},
 // });
 
+// frappe.call({
+//     method: "vms.otp_verification.verify_otp_and_delete",
+//     args: {
+//         docname: "OTP-00001",
+//         input_otp: "123456"
+//     },
+//     callback: function(response) {
+//         if (response.message.status === "success") {
+//             frappe.msgprint(response.message.message);
+//         }
+//     }
+// });
