b901f853f4101151982d98926035ed8dc5e11ba0
RahulSarkar <bluephoenix00995@gmail.com>
2025-05-12 17:38:28 +0530

    feat: otp exired and deleted

--- vms/vms/doctype/otp_verification/otp_verification.py
+++ vms/vms/doctype/otp_verification/otp_verification.py
@@ -4,9 +4,86 @@
 import frappe
 import time
 from frappe.model.document import Document
+from frappe.utils.background_jobs import enqueue
 
 class OTPVerification(Document):
-    pass
+    def after_insert(self):
+        exp_doc = frappe.get_doc("OTP Settings") or None
+        print("@@@@@@@@@@@@@@@@@", exp_doc)
+        
+        if exp_doc != None:
+            exp_t_min = float(exp_doc.otp_expiration_time)
+            print("@@@@@@@@@@@@@@@@@", exp_t_min)
+            exp_t_sec = exp_t_min * 60
+            print("@@@@@@@@@@@@@@@@@", exp_t_sec)
+        else:
+            exp_t_sec = 300
+            
+        # Enqueue a background job to handle OTP expiration
+        exp_d_sec = exp_t_sec + 300
+        frappe.enqueue(
+            method=self.handle_otp_expiration,
+            queue='default',
+            timeout=exp_d_sec,
+            now=False,
+            job_name=f'otp_expiration_{self.name}',
+            # enqueue_after_commit = False
+        )
+
+        frappe.enqueue(
+            method=self.delete_otp_doc,
+            queue='default',
+            timeout=exp_d_sec,
+            now=False,
+            job_name=f'otp_deletion_{self.name}',
+            # enqueue_after_commit = False
+        )
+        
+    def handle_otp_expiration(self):
+        # This will run in the background after the specified time
+        # Put your OTP expiration logic here
+        # frappe.delete_doc("OTP Verification", self.name, force = 1)
+        # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ OTP expiration job executed")
+        # frappe.db.commit()
+        
+        # For example, you might want to update the status of the OTP verification
+        exp_doc = frappe.get_doc("OTP Settings") or None
+        
+        if exp_doc != None:
+            exp_t_min = float(exp_doc.otp_expiration_time)
+            exp_t_sec = exp_t_min * 60
+           
+        else:
+            exp_t_sec = 300
+        time.sleep(exp_t_sec)
+        
+        self.db_set('expired', 1, update_modified=False)
+
+        # exp_d_sec = exp_t_sec + 300
+        frappe.db.commit()
+
+        
+
+    def delete_otp_doc(self):
+        exp_doc = frappe.get_doc("OTP Settings") or None
+        if exp_doc != None:
+            exp_t_min = float(exp_doc.otp_expiration_time)
+            exp_t_sec = exp_t_min * 60
+           
+        else:
+            exp_t_sec = 300
+        
+
+        exp_d_sec = exp_t_sec + 120
+        time.sleep(exp_d_sec)
+        
+        frappe.delete_doc("OTP Verification", self.name, force = 1)
+        frappe.db.commit()
+
+
+
+        
+
 
 
 @frappe.whitelist(allow_guest = True)
