dae58a9baa5af0b8afe872acd282be0058a3d43c
RahulSarkar <bluephoenix00995@gmail.com>
2025-05-23 17:44:16 +0530

    feat: all po data for all details

--- vms/APIs/dashboard_api/po_data.py
+++ vms/APIs/dashboard_api/po_data.py
@@ -6,3 +6,11 @@ import json
 def get_po():
     all_po = frappe.get_all("Purchase Order", fields ="*")
     return all_po
+
+
+
+@frappe.whitelist(allow_guest = True)
+def get_po_details(data):
+    po_name = data.get("po_name")
+    po = frappe.get_doc("Purchase Order", po_name)
+    return po.as_dict()
