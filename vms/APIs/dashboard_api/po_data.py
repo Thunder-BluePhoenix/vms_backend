4a0ba5628d061226b4bd2197a1b4cdc717380667
abhishek-chavan-01 <iamabhichavan123@gmail.com>
2025-06-04 10:15:07 +0530

    feat: made default pending status in ven onboarding

--- vms/APIs/dashboard_api/po_data.py
+++ vms/APIs/dashboard_api/po_data.py
@@ -14,3 +14,7 @@ def get_po_details(data):
     po_name = data.get("po_name")
     po = frappe.get_doc("Purchase Order", po_name)
     return po.as_dict()
+
+@frappe.whitelist(allow_guest = True)
+def filtering_data(data):
+    pass
