bcfbe4e1be9ea6a1ab68a5ecbb17d87842ee7fb7
RahulSarkar <bluephoenix00995@gmail.com>
2025-06-05 16:55:09 +0530

    feat: sap code check and validate

--- vms/vendor_onboarding/doctype/legal_documents/legal_documents.py
+++ vms/vendor_onboarding/doctype/legal_documents/legal_documents.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2025, Blue Phoenix and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class LegalDocuments(Document):
-	pass
+	def on_update(self):
+		
+		vonb_comp_name = frappe.db.get_value(
+			"Vendor Onboarding Company Details",
+			{"ref_no": self.ref_no, "vendor_onboarding": self.vendor_onboarding},
+			"name"
+		)
+
+		vonb_comp = frappe.get_doc("Vendor Onboarding Company Details", vonb_comp_name)
+
+		vonb_comp.gst = self.gst_table[0].gst_number
+		vonb_comp.save()
+		frappe.db.commit()
