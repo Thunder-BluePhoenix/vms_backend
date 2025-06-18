99588c7cdf9499c01488e8a67ccab3ae21cd1cb8
abhishek-chavan-01 <iamabhichavan123@gmail.com>
2025-06-18 13:31:08 +0530

    fix: purchase team api for upload bank details

--- vms/assessment_forms/doctype/supplier_qms_assessment_form/supplier_qms_assessment_form.py
+++ vms/assessment_forms/doctype/supplier_qms_assessment_form/supplier_qms_assessment_form.py
@@ -7,8 +7,15 @@ from frappe.model.document import Document
 
 class SupplierQMSAssessmentForm(Document):
 	def on_update(self):
-		if self.vendor_onboarding:
-			ven_onb = frappe.get_doc("Vendor Onboarding", self.vendor_onboarding)
-			ven_onb.qms_form_link = self.name
-			ven_onb.save()
-			frappe.db.commit()
+		set_qms_form_link(self, method=None)
+
+
+
+
+@frappe.whitelist(allow_guest=True)
+def set_qms_form_link(doc, method=None):
+	if doc.vendor_onboarding:
+		ven_onb = frappe.get_doc("Vendor Onboarding", doc.vendor_onboarding)
+		ven_onb.qms_form_link = doc.name
+		ven_onb.save()
+		frappe.db.commit()
