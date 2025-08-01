47f2cd4788e9dbf6e4986691b5541c2fd40cc450
tusharbhombe2001 <tushar@mail.hybrowlabs.com>
2025-08-01 16:05:10 +0530

    added earth invoice approval flow

--- vms/vms/doctype/earth_invoice/earth_invoice.py
+++ vms/vms/doctype/earth_invoice/earth_invoice.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2025, Blue Phoenix and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class EarthInvoice(Document):
-	pass
+    def validate(self):
+        self.update_upload_status()
+    
+    def update_upload_status(self):
+        attach_fields = ['confirmation_voucher', 'invoice_attachment', 'debit_note_attachment']
+        
+        uploaded_count = 0
+        for field in attach_fields:
+            if self.get(field):
+                uploaded_count += 1
+        
+        if uploaded_count == 3:
+            self.doc_upload_status = "Fully Uploaded"
+        elif uploaded_count > 0:
+            self.doc_upload_status = "Partially Uploaded"
+        else:
+            self.doc_upload_status = "Not Uploaded"
