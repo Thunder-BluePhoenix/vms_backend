dfe85cfba891f63622e10ff9726379aba59deac5
abhishek-chavan-01 <iamabhichavan123@gmail.com>
2025-10-17 11:39:42 +0530

    fix : Email for Approval of Purchase Order and set limit 20 for every pagination

--- vms/APIs/master_apis/valuation_class_master.py
+++ vms/APIs/master_apis/valuation_class_master.py
@@ -4,7 +4,7 @@ import json
 @frappe.whitelist(allow_guest=True)
 def get_valuation_class_list(limit=None, offset=0, order_by=None, search_term=None):
     try:
-        limit = int(limit) if limit else 10
+        limit = int(limit) if limit else 20
         offset = int(offset) if offset else 0
         
         order_by = order_by if order_by else "creation desc"
