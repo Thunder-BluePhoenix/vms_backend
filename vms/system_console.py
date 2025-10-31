61b6c6e4b0722eec21e2141330cb9e2fe7ba6927
Thunder-BluePhoenix <bluephoenix00995@gmail.com>
2025-10-31 12:42:17 +0530

    feat: role and permission setup for mgm,valuation and gl acc and system console update

--- vms/system_console.py
+++ vms/system_console.py
@@ -8,3 +8,33 @@
 #     new_name = f"{comp.company_code}-{comp.company_name}"
 #     if comp.name != new_name:
 #         frappe.rename_doc("Company Master", comp.name, new_name, force=1, merge=False)
+
+
+
+
+# duplicates = frappe.db.sql("""
+#     SELECT gl_account_code, COUNT(*) as count
+#     FROM `tabGL Account`
+#     WHERE gl_account_code IS NOT NULL AND gl_account_code != ''
+#     GROUP BY gl_account_code
+#     HAVING COUNT(*) > 1
+# """, as_dict=True)
+
+# if not duplicates:
+#     print("✅ No duplicate employee numbers found.")
+# else:
+#     print("⚠️ Duplicate Employee Numbers:")
+#     for d in duplicates:
+#         print(f"{d.gl_account_code} → {d.count} records")
+
+#     # (Optional) To list the specific employees for each duplicate
+#     print("\nDetailed list of duplicates:")
+#     for d in duplicates:
+#         employees = frappe.db.get_all(
+#             "GL Account",
+#             filters={"gl_account_code": d.gl_account_code},
+#             fields=["name", "employee_name", "gl_account_code"]
+#         )
+#         print(f"\nEmployee Number: {d.gl_account_code}")
+#         for e in employees:
+#             print(f" - {e.name} | {e.employee_name}")
